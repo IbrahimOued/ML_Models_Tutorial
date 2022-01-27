10ab70a0f2d82f4f94a446760d6da00c0b3a7610
Ibrah <brahim.oued@gmail.com>
2022-01-27 01:43:01 +0000

    train model: RandomForestRegressor

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,7 +1,7 @@
 from operator import mod
 import pandas as pd
 import pickle
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from config import Config
 
 Config.MODELS_PATH.mkdir(parents=True, exist_ok=True)
@@ -10,7 +10,8 @@ X_train = pd.read_csv(str(Config.FEATURES_PATH/"train_features.csv"))
 y_train = pd.read_csv(str(Config.FEATURES_PATH/"train_labels.csv"))
 
 # Entrainement du modèle
-model = LinearRegression()
+model = RandomForestRegressor(
+    n_estimators=150, max_depth=5, random_state=Config.RANDOM_SEED)
 model.fit(X_train, y_train.to_numpy().ravel())
 # Enregistrement du modèle
 pickle.dump(model, open(str(Config.MODELS_PATH / "model.pk"), mode='wb'))
